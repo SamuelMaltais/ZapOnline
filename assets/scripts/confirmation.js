b73b3aed3917c449a004c563949a487b33fdfac6
Félix Beaudoin <56303117+Avunz@users.noreply.github.com>
2022-12-11 15:53:50 -0500

    confirmation.html fini
    dans order.html, il faut save le nom et un client number sous : confirmationNumber et clientName

---
+++ assets/scripts/confirmation.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+    
+    localStorage.setItem("clientName", "clientName")
+    localStorage.setItem("confirmationNumber", "confirmationNumber")
+
+    let clientName = localStorage.getItem("clientName")
+    let confirmationNumber = localStorage.getItem("confirmationNumber")
+
+    $("#confirmation-number").html(confirmationNumber)
+    $("#client-name").html(clientName)
+}
+
+)
